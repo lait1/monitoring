842b71781f2aace56e0a54a13020b0c9a9b7a686
alex <ale88371027@yandex.ru>
2018-12-29 14:12:18 +0500

    Верстка главной страницы и модальных окон

--- index.php
+++ index.php
@@ -9,20 +9,136 @@ include 'options.php';
 	<link href="css/bootstrap.min.css" rel="stylesheet">
 	<meta charset="UTF-8">
 	<title>Document</title>
-	<script src="js/bootstrap.min.js"></script>
 	<script src="js/jquery-3.3.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	
 </head>
 <body>
-<h1>Lorem ipsum dolor sit amet</h1>
-<form class="main_form"  method="post">
-	<label for="login">Имя пользователя</label><br>
-	<input type="text"  name="login" id="login" required><Br>
+<div class="container">
+	<div class="row">
+	<h1 class="main_title">Мониторинг раздач</h1></div>
+	<div class="row">
+		<div class="col-md-9">
+			<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#add_tracker">Добавить трекер</button>
+			</div>
+		<div class="col-md-3">
+			<button type="button" class="btn btn-info" data-toggle="modal" data-target="#add_link">Добавить раздачу</button>
+			</div>
+	</div>
+	<p>
+	      
+	      
+	</p>
+	 <table class="table table-striped table-bordered table-hover">
+	      <thead>
+	        <tr>
+	          <th>#</th>
+	          <th>Наименование</th>
+	          <th>Обновление</th>
+	          <th>Скачать</th>
+	          <th>Удалить</th>
+	        </tr>
+	      </thead>
+	      <tbody>
+	        <tr>
+	          <td>1</td>
+	          <td>Барыги: Мексика</td>
+	          <td>29 ДЕКАБРЯ 2018</td>
+	          <td>Скачать</td>
+	          <td>Удалить</td>
+	        </tr>
+	        <tr>
+	          <td>2</td>
+	          <td>Страна приливов</td>
+	          <td>29 ДЕКАБРЯ 2018</td>
+	          <td>Скачать</td>
+	          <td>Удалить</td>
+	        </tr>
+	        <tr>
+	          <td>3</td>
+	          <td>1Викинги</td>
+	          <td>29 ДЕКАБРЯ 2018</td>
+	          <td>Скачать</td>
+	          <td>Удалить</td>
+	        </tr>
+	      </tbody>
+	    </table>
+</div>	
+<!-- Modal 1 -->
+<div class="modal fade" id="add_tracker" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title" id="myModalLabel">Добавить Трекер</h4>
+      </div>
+      <div class="modal-body">
+		  <form class="form-horizontal" role="form">
+		  	<div class="form-group">
+			    <label for="inputNameTracker" class="col-sm-2 control-label">Название</label>
+			    <div class="col-sm-10">
+			      <input type="text" class="form-control" id="inputNameTracker" placeholder="Название">
+			    </div>
+			  </div>
+			  <div class="form-group">
+			    <label for="inputLogin" class="col-sm-2 control-label">URL</label>
+			    <div class="col-sm-10">
+			      <input type="url" class="form-control" id="inputUrlTracker" placeholder="Ссылка">
+			    </div>
+			  </div>
 
-	<label for="password">Пароль</label><br>
-	<input type="password"  name="password" id="password" required><Br>
+			  <div class="form-group">
+			    <label for="inputLogin" class="col-sm-2 control-label">Login</label>
+			    <div class="col-sm-10">
+			      <input type="text" class="form-control" id="inputLogin" placeholder="Login">
+			    </div>
+			  </div>
+			  <div class="form-group">
+			    <label for="inputPassword" class="col-sm-2 control-label">Пароль</label>
+			    <div class="col-sm-10">
+			      <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+			    </div>
+			  </div>
+		  </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Закрыть</button>
+        <button type="button" class="btn btn-primary">Сохранить</button>
+      </div>
+    </div>
+  </div>
+</div>
 
-	<input type=submit value=Выбрать name="submit">
-</form>
-<p><a href="register.php">Зарегистрироваться</a></p>
+<!-- Moda2 -->
+<div class="modal fade" id="add_link" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title" id="myModalLabel">Добавить раздачу</h4>
+      </div>
+      <div class="modal-body">
+        <form class="form-horizontal" role="form">
+		  	<div class="form-group">
+			    <label for="inputNameTracker" class="col-sm-2 control-label">Название</label>
+			    <div class="col-sm-10">
+			      <input type="text" class="form-control" id="inputNameTracker" placeholder="Название">
+			    </div>
+			  </div>
+			  <div class="form-group">
+			    <label for="inputLogin" class="col-sm-2 control-label">URL</label>
+			    <div class="col-sm-10">
+			      <input type="url" class="form-control" id="inputUrlTracker" placeholder="Ссылка">
+			    </div>
+			  </div>
+		  </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Закрыть</button>
+        <button type="button" class="btn btn-primary">Сохранить</button>
+      </div>
+    </div>
+  </div>
+</div>
 </body>
 </html>
