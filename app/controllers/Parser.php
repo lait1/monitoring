48c67ff30981dab7b4c494d8f1c9fa2474d98206
alex <ale88371027@yandex.ru>
2019-01-15 19:23:42 +0500

    Начало парсинга алексфильма

--- app/controllers/Parser.php
+++ app/controllers/Parser.php
@@ -15,31 +15,34 @@ class Parser extends Controller {
 		foreach ($link as $data) {
 			$url = $data->Distrib_link;
 			$tracker = Tracker::getTracker($data->Id_link);
-			///////////////////////////////
-			//На будущее, проверяем какой трекер и от этого выбирается вид парсера
-			//////////////////////////////
+		///////////////////////////////
+		//На будущее, проверяем какой трекер и от этого выбирается вид парсера
+		//////////////////////////////
 			$html = Model_Parser::Read($url, $tracker->cookies);
-			// Получение новой даты
+		// Получение новой даты
 			$newDate= Model_Parser::get_link_date($html);
+		// Сравнение текущей даты с новой
 			if($newDate > $data->last_update){
+		// Обновление даты на актуальную
 				$UpdateLinks=Link::UpdateLink($data->Id_link, $newDate);
-					if (!$UpdateLinks) {
-						$error=true;
-					}
-				$episode= Model_Parser::get_link_episode($html, $tracker->Link_track);
 
+				if (!$UpdateLinks) {
+					$error=true;
+				}
+		// Получение ссылки на эпизод
+				$episode= Model_Parser::get_link_episode($html, $tracker->Link_track);
+		// Чтение страницы по новой ссылке и получение уникального кода серии
 				$html = Model_Parser::Read($episode, $tracker->cookies);
 				$link_code = Model_Parser::get_link_code_episode($html);
-
+		//По уникальному коду серии получаем промежуточную ссылку на торрент 
 				$link_torrent_1 ='http://www.lostfilm.tv/v_search.php?a='.$link_code;
 				$html = Model_Parser::Read($link_torrent_1, $tracker->cookies);
 				$link_torrent_2 = Model_Parser::get_link_torrent($html, '0');
-
+		// Получаем итоговую ссылку на торрент
 				$html = Model_Parser::Read($link_torrent_2, $tracker->cookies);
 				$link_torrent_real = Model_Parser::get_link_torrent($html, '3');
-
+		// Обновляем в базе ссылку на новый торрент
 				$UpdateLinkTorrent=Link::UpdateLinkTorrent($data->Id_link, $link_torrent_real);
-
 				if (!$UpdateLinkTorrent) {
 						$error=true;
 					}
--- app/models/Parser.php
+++ app/models/Parser.php
@@ -22,6 +22,26 @@ public static function Read($url, $cookie, $post=0){
    return $result;
 }
 
+public static function ReadProxy($url, $cookie, $type=0){
+
+   $ch = curl_init();
+   curl_setopt($ch, CURLOPT_URL, $url);
+   curl_setopt($ch, CURLOPT_HEADER, 0); 
+   curl_setopt($ch, CURLOPT_REFERER, $url);
+   curl_setopt($ch, CURLOPT_PROXY, '66.96.200.39:80');
+   if(!empty($type)) 
+   		curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5);
+   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+   // curl_setopt($ch, CURLOPT_POST, $post!==0);
+   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+   curl_setopt($ch, CURLOPT_COOKIE, $cookie);
+   curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (Windows; U; Windows NT 5.0; En; rv:1.8.0.2) Gecko/20070306 Firefox/1.0.0.4");
+	// curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+   $result = curl_exec($ch);
+   curl_close($ch);
+
+   return $result;
+}
 
 	public static function get_link_date( $data ) {
 			$html = new simple_html_dom();
